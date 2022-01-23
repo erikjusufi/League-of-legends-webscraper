e464b594b5a556a3cd111ab29eb76cc620e391f6
erikjusufi <jusufi.erik@gmail.com>
2022-01-23 21:28:14 +0100

    update

--- player_games/page_player_games.py
+++ player_games/page_player_games.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 file = open("page.txt", "w", encoding="utf-8")
-driver.get("https://www.probuilds.net/pros/details/6416")
+driver.get("https://www.probuilds.net/teams/details/189")
 file.write(driver.page_source)
-driver.close()
+
 file.close()
