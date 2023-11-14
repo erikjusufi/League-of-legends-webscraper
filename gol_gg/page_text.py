b6e776e33a8e3b0f0ea74d23c8c95dddf9d98eb3
Erik Jusufi <jusufi.erik@gmail.com>
2023-11-14 12:32:19 +0100

    files organized

--- gol_gg/page_text.py
+++ gol_gg/page_text.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-file = open("gol_gg.txt", "w", encoding="utf-8")
+file = open("gol_gg_text.txt", "w", encoding="utf-8")
 driver.get("https://gol.gg/teams/list/season-S12/split-Spring/tournament-ALL/")
 file.write(driver.page_source)
 driver.close()
